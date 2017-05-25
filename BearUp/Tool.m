0687164f41a328ac7bc67e4e26c8e347348570cf
Lee2Go <hey2li@163.com>
2017-05-25 15:29:17 +0800

    。。

--- BearUp/Tool.m
+++ BearUp/Tool.m
@@ -85,5 +85,17 @@ NSAttributedString *returnNumAttr(NSString *str,NSInteger fontSize){
     NSLog(@"signStr=%@,\nMD5=%@\n",signStr,md5Str);
     return params;
 }
-
++ (NSString *)MD5:(NSString *)str{
+    //要进行UTF8的转码
+    const char* input = [str UTF8String];
+    unsigned char result[CC_MD5_DIGEST_LENGTH];
+    CC_MD5(input, (CC_LONG)strlen(input), result);
+    
+    NSMutableString *digest = [NSMutableString stringWithCapacity:CC_MD5_DIGEST_LENGTH * 2];
+    for (NSInteger i = 0; i < CC_MD5_DIGEST_LENGTH; i++) {
+        [digest appendFormat:@"%02x", result[i]];
+    }
+    
+    return digest;
+}
 @end
